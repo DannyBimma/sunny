8bbcbb92e9a308cbd9cc29852f35927b2dc2f81e
DannyBimma <dantrotman92@gmail.com>
2025-10-24 21:11:24 -0400

    Init program file.

---
+++ sunny.swift
@@ -0,0 +1,7 @@
+/*
+Routine: A Simulation of the tech on-board the Thousand Sunny pirate ship,
+from the One Piece manga and anime.
+Author: Danny Bimma
+Date: 2025-10-24
+Copyright: 2025 Technomancer Pirate Captain
+*/
