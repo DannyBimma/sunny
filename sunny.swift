36a4c16e99743c32b2c9e4f9191f1f68a8225ee5
DannyBimma <dantrotman92@gmail.com>
2025-10-24 21:28:55 -0400

    Start modeling Soldier Docking System.

--- sunny.swift
+++ sunny.swift
@@ -5,3 +5,14 @@ Author: Danny Bimma
 Date: 2025-10-24
 Copyright: 2025 Technomancer Pirate Captain
 */
+
+// Create an enum for the Soldier Dock System
+enum SoldierDock: Int {
+    // Dock contents
+    case shiroMokuba = 1
+    case miniMerry
+    case sharkSubmerge
+    case kruosaiIV
+    case brachioTankV
+    case inflatablePool
+}
