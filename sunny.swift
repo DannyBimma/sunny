33810a1465d1d1ded3015614aae6dc29efa83a73
DannyBimma <dantrotman92@gmail.com>
2025-10-24 22:01:15 -0400

    Add method to display vehicle info.

--- sunny.swift
+++ sunny.swift
@@ -15,4 +15,34 @@ enum SoldierDock: Int {
     case kruosaiIV
     case brachioTankV
     case inflatablePool
+
+    // Show info about a specific dock channels
+    func displayInfo(for dock: SoldierDock) {
+        switch dock {
+        case .shiroMokuba:
+            print("Channel 1 - Shiro Mokuba:")
+            print(
+                "Waver owned by Nami, in the shape of a white horse. Orginally salvaged from a shipwreck in Jaya."
+            )
+        case .miniMerry:
+            print("Channel 2 - Mini Merry:")
+            print(
+                "Small 4-person boat resembling the Going Merry. Built by Franky at Nami's request, for the exlusive use of shopping."
+            )
+        case .sharkSubmerge:
+            print("Channel 3 - Shark Submerge:")
+            print("3-person submersible shaped like a shark.")
+        case .kruosaiIV:
+            print("Channel 4 - Kurosai IV:")
+            print(
+                "Giant black motorcycle with 3-wheels and the head of a Rhinoceros. Built by Franky for unknown reasons."
+            )
+        case .brachioTankV:
+            print("Channel 5 - Brachio Tank V:")
+            print("Giant tank shaped like a Brachiosaurus. Built by Franky for obvious reasons.")
+        case .inflatablePool:
+            print("Channel 6 - Inflatable Pool:")
+            print("Large inflatable pool for relaxation purposes.")
+        }
+    }
 }
